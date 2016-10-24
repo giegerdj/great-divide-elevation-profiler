a4ef7e1f4239b7093c38a52019ba286a4afbe35d
Dave Gieger <gieger.david@gmail.com>
2016-10-24 01:22:21 +0000

    update key file

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@ role :db, "p.davegieger.com", :primary => true
 
 set :deploy_to, "/var/www/html/master/great-divide-elevation-profiler/"
 
-ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "prod_deploy_dsa")]
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "prod_deploy_rsa")]
 
 set :branch, "master"
 
